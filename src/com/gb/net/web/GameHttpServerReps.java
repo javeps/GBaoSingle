b2f37d49a1611d9d95cbd2d6c27244d0455467da
Drifter <379657505@qq.com>
2016-07-08 15:42:02 +0800

    修改了上传文件的方式
    可以上传文件

--- src/com/gb/net/web/GameHttpServerReps.java
+++ src/com/gb/net/web/GameHttpServerReps.java
@@ -3,7 +3,9 @@ package com.gb.net.web;
 import gen_b2g.web_disp.GBSngGmSvI;
 import io.netty.channel.Channel;
 import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.multipart.FileUpload;
 
+import java.io.File;
 import java.io.Serializable;
 import java.net.URI;
 import java.util.Date;
@@ -13,6 +15,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.bowlong.bio2.B2Helper;
+import com.bowlong.io.FileRw;
 import com.bowlong.lang.PStr;
 import com.bowlong.lang.StrEx;
 import com.bowlong.third.FastJSON;
@@ -21,6 +24,7 @@ import com.bowlong.third.netty4.httphand.N4HttpResponse;
 import com.bowlong.util.DateEx;
 import com.bowlong.util.MapEx;
 import com.bowlong.util.NewMap;
+import com.gb.content.AppContext;
 import com.gb.content.Svc;
 import com.gb.db.bean.Player;
 import com.gb.logic.impl.GBSngGmImpl;
@@ -96,6 +100,12 @@ public class GameHttpServerReps implements Serializable {
 				case "/seePlByName":
 					seePlByName(chn, msg);
 					break;
+				case "/upJson":
+					upJson(chn, msg);
+					break;
+				case "/upFile":
+					upFile(chn, msg);
+					break;
 				default:
 					N4HttpResponse.send(chn, Out_Error + ",该方法名字有误:" + path);
 					break;
@@ -134,7 +144,7 @@ public class GameHttpServerReps implements Serializable {
 		}
 	}
 
-	void rnkPlayer(Channel chn, Object msg) {
+	void rnkPlayer(Channel chn, Object msg) throws Exception {
 		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
 		// 是否重新生成当天的排行榜
 		boolean isRnk = MapEx.getBoolean(map, "isRnk");
@@ -142,158 +152,97 @@ public class GameHttpServerReps implements Serializable {
 		String str = MapEx.getString(map, "time");
 		// 是否生成排行奖励邮件
 		boolean isEmail = MapEx.getBoolean(map, "isEmail");
-		try {
-			TimerNight.getInstance().exce4Pub(isRnk, str, isEmail);
-			N4HttpResponse.send(chn, "重新排行生成!");
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+		TimerNight.getInstance().exce4Pub(isRnk, str, isEmail);
+		N4HttpResponse.send(chn, "重新排行生成!");
 	}
 
-	void rnkReset4Pl(Channel chn, Object msg) {
+	void rnkReset4Pl(Channel chn, Object msg) throws Exception {
 		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
 		String unqid = MapEx.getString(map, "unqid");
 		String name = MapEx.getString(map, "name");
-		try {
-			boolean isOkey = Logical.resetActivity(unqid, name);
-			if (isOkey) {
-				N4HttpResponse.send(chn, "重新设置角色的活动状态，成功!");
-			} else {
-				N4HttpResponse.send(chn, "重新设置角色的活动状态，失败!");
-			}
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
+		boolean isOkey = Logical.resetActivity(unqid, name);
+		if (isOkey) {
+			N4HttpResponse.send(chn, "重新设置角色的活动状态，成功!");
+		} else {
+			N4HttpResponse.send(chn, "重新设置角色的活动状态，失败!");
 		}
 	}
 
-	void rnkAward4Send(Channel chn, Object msg) {
+	void rnkAward4Send(Channel chn, Object msg) throws Exception {
 		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
 		String str = MapEx.getString(map, "time");
 		boolean isSend = MapEx.getBoolean(map, "isSend");
-		try {
-			OptSvEmail4Rnk.setDataStr4Get(str, isSend);
-			N4HttpResponse.send(chn, "重新设置取得排行榜数据!取得表的时间:" + str + ",是否可以取得:"
-					+ isSend);
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+		OptSvEmail4Rnk.setDataStr4Get(str, isSend);
+		N4HttpResponse.send(chn, "重新设置取得排行榜数据!取得表的时间:" + str + ",是否可以取得:"
+				+ isSend);
 	}
 
 	/*** 创建排名奖励邮件 **/
-	void createEmail4RnkByJsonTxt(Channel chn, Object msg) {
+	void createEmail4RnkByJsonTxt(Channel chn, Object msg) throws Exception {
 		// FileUpload fup = N4HttpResp.getFileByMsg(msg);
 		// byte[] buf = N4B2ByteBuf.readBuff(fup.content());
-		try {
-			Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
-			String str = MapEx.getString(map, "json");
-			Map mapU_Ind = FastJSON.parseMap(str);
-			OptSvEmail4Rnk.createEmails4RnkByMap(mapU_Ind);
-			N4HttpResponse.send(chn, "成功！");
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
+		String str = MapEx.getString(map, "json");
+		Map mapU_Ind = FastJSON.parseMap(str);
+		OptSvEmail4Rnk.createEmails4RnkByMap(mapU_Ind);
+		N4HttpResponse.send(chn, "成功！");
 	}
 
 	/*** 创建邮件 **/
-	void createEmail(Channel chn, Object msg) {
+	void createEmail(Channel chn, Object msg) throws Exception {
 		// FileUpload fup = N4HttpResp.getFileByMsg(msg);
 		// byte[] buf = N4B2ByteBuf.readBuff(fup.content());
-		try {
-			Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
-			String unqid = MapEx.getString(map, "unqid");
-			String title = MapEx.getString(map, "title");
-			String cont = MapEx.getString(map, "cont");
-			String awardJson = MapEx.getString(map, "awardJson");
-			OptSvEmail4Rnk.createEmail4Pl(unqid, title, cont, awardJson);
-			N4HttpResponse.send(chn, "成功！");
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+
+		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
+		String unqid = MapEx.getString(map, "unqid");
+		String title = MapEx.getString(map, "title");
+		String cont = MapEx.getString(map, "cont");
+		String awardJson = MapEx.getString(map, "awardJson");
+		OptSvEmail4Rnk.createEmail4Pl(unqid, title, cont, awardJson);
+		N4HttpResponse.send(chn, "成功！");
 	}
 
 	/*** 根据渠道排行 **/
-	void rnk4Chn(Channel chn, Object msg) {
+	void rnk4Chn(Channel chn, Object msg) throws Exception {
 		// FileUpload fup = N4HttpResp.getFileByMsg(msg);
 		// byte[] buf = N4B2ByteBuf.readBuff(fup.content());
-		try {
-			Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
-			String chnStr = MapEx.getString(map, "chn");
-			int type = MapEx.getInt(map, "type");
-			int parsType = MapEx.getInt(map, "parsType");
-			TimerNight.getInstance().exce4Chn(chnStr, type, parsType);
-			N4HttpResponse.send(chn, chnStr + "成功！");
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
+		String chnStr = MapEx.getString(map, "chn");
+		int type = MapEx.getInt(map, "type");
+		int parsType = MapEx.getInt(map, "parsType");
+		TimerNight.getInstance().exce4Chn(chnStr, type, parsType);
+		N4HttpResponse.send(chn, chnStr + "成功！");
 	}
 
-	void seePlByName(Channel chn, Object msg) {
-		try {
-			Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
-			String strName = MapEx.getString(map, "name");
-			Player pl = Logical.getPlByName(strName);
-			if (pl != null) {
-				N4HttpResponse.send(chn, pl.toJson(true));
-			} else {
-				N4HttpResponse.send(chn, "play is null,name = " + strName);
-			}
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
+	void seePlByName(Channel chn, Object msg) throws Exception {
+		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
+		String strName = MapEx.getString(map, "name");
+		Player pl = Logical.getPlByName(strName);
+		if (pl != null) {
+			N4HttpResponse.send(chn, pl.toJson(true));
+		} else {
+			N4HttpResponse.send(chn, "play is null,name = " + strName);
 		}
 	}
 
 	/*** 是否清除日志 **/
-	void isClearLog(Channel chn, Object msg) {
+	void isClearLog(Channel chn, Object msg) throws Exception {
 		// FileUpload fup = N4HttpResp.getFileByMsg(msg);
 		// byte[] buf = N4B2ByteBuf.readBuff(fup.content());
-		try {
-			Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
-			boolean isClear = MapEx.getBoolean(map, "isClear");
-			TimerNight.isClearLog = isClear;
-			N4HttpResponse.send(chn, "成功！");
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+
+		Map<String, String> map = N4HttpResp.getMapKVByMsg(msg);
+		boolean isClear = MapEx.getBoolean(map, "isClear");
+		TimerNight.isClearLog = isClear;
+		N4HttpResponse.send(chn, "成功！");
 	}
 
 	/*** IOS_充值回调验证 **/
-	void verify_ios_pay(Channel chn, Object msg) {
-		try {
-			Map<String, String> mapData = N4HttpResp.getMapKVByMsg(msg);
-			String sign = MapEx.getString(mapData, "sign");
-			boolean ret = IAPIOSRecharge.checkPay(mapData, sign);
-			// byte[] bts = ByteEx.putBoolean(ret);
-			N4HttpResponse.send(chn, String.valueOf(ret));
-		} catch (Exception e) {
-			try {
-				N4HttpResponse.send(chn, Svc.e2s(e));
-			} catch (Exception ee) {
-			}
-		}
+	void verify_ios_pay(Channel chn, Object msg) throws Exception {
+		Map<String, String> mapData = N4HttpResp.getMapKVByMsg(msg);
+		String sign = MapEx.getString(mapData, "sign");
+		boolean ret = IAPIOSRecharge.checkPay(mapData, sign);
+		// byte[] bts = ByteEx.putBoolean(ret);
+		N4HttpResponse.send(chn, String.valueOf(ret));
 	}
 
 	/*** 游戏请求方法 **/
@@ -324,5 +273,32 @@ public class GameHttpServerReps implements Serializable {
 
 		System.out.println(info);
 		// log.info(info);
+	}
+
+	void upJson(Channel chn, Object msg) throws Exception {
+		String t = FileRw.readStr("html/upFile.html");
+		StringBuilder builder = new StringBuilder("http://");
+		String host = AppContext.getGateHost();
+		if (StrEx.isEmpty(host)) {
+			// host = "112.124.56.63";
+			host = "127.0.0.1";
+		}
+		builder.append(host);
+		builder.append(":");
+		builder.append(AppContext.getGamePortWeb());
+		builder.append("/upFile");
+		String action = builder.toString();
+		t = StrEx.fmt(t, action);
+		N4HttpResponse.send(chn, t);
+	}
+
+	void upFile(Channel chn, Object msg) throws Exception {
+		FileUpload fup = N4HttpResp.getFileByMsg(msg);
+		String fileName = fup.getFilename();
+		String suffix = StrEx.right(fileName, ".");
+		String newFilePath = "files/" + DateEx.nowStr4() + "." + suffix;
+		File f = FileRw.getFile(newFilePath);
+		fup.renameTo(f);
+		N4HttpResponse.send(chn, "okey");
 	}
 }
