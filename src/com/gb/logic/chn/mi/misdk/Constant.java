25b10ae22c11468f1729db7942ce013a5ba86e19
Drifter <379657505@qq.com>
2016-09-04 21:23:38 +0800

    修改小米

--- src/com/gb/logic/chn/mi/misdk/Constant.java
+++ src/com/gb/logic/chn/mi/misdk/Constant.java
@@ -2,8 +2,8 @@ package com.gb.logic.chn.mi.misdk;
 
 public class Constant {
 
-	public static final long APP_ID = 2882303761517239138l;
-	public static final String SECRET_KEY = "VcIH/gYkcr7B7k2FUT3J5g==";
+	public static final long APP_ID = 2882303761517505874l;
+	public static final String SECRET_KEY = "b6S145yYq8zR2+AsriNnKw==";
 	
 	public static final int CODE_SUCCESS = 200;
 	public static final int CODE_SYS_ERROR = 1001;
